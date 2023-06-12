5025fac4d825d3add0abede66be85a8a339f6c9b
Dheerajreddy9999 <ksreddy.dheeraj@gmail.com>
2023-06-12 09:46:53 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+}
+
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-024e6efaf93d85776"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "web-servers"
+    app = "jenkins"
+  }
+}
+
